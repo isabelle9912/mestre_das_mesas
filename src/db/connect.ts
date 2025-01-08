e83c0fd38d1b56d48d27a5a295a44f83bc813d2e
isabelle9912 <isabellesaahirah28@gmail.com>
2025-01-08 15:41:09 -0300

    feat: file that control the conection with date base

---
+++ src/db/connect.ts
@@ -0,0 +1,33 @@
+import "dotenv/config";
+
+const dbHost = process.env.PGHOST!;
+const dbName = process.env.PGDATABASE!;
+const dbUser = process.env.PGUSER!;
+const dbPassword = process.env.PGPASSWORD!;
+
+console.log(dbName);
+
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
+  dialect: "postgres",
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Para resolver erros de certificado SSL
+    },
+  },
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Conectado com o banco!");
+  } catch (error) {
+    console.error(`Não foi possível conectar: ${error}`);
+  }
+})();
+
+export default sequelize;
